0737b06185046db2b46a90be28555b3439a1937b
EricSherlockLi <helloleolee@hotmail.com>
2023-01-03 08:46:16 +0800

    更改todo item状态

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import List from './components/List'
 import './App.css';
 
 export default class App extends Component {
+  // 状态在哪里，操作状态的方法就在哪里
 
+  // 初始化状态
   state = {todos: [
       { id: '001', name: 'Eating', done: true },
       { id: '002', name:'Sleeping', done:true},
@@ -23,14 +25,26 @@ export default class App extends Component {
     this.setState({todos:newTodos})
   }
 
+  // updateTodo用于更新一个todo对象
+  updateTodo = (id, done) => {
+    // 获取状态中的todos
+    const { todos } = this.state
+    // 匹配处理数据
+    const newTodos = todos.map((todoObj) => {
+      if (todoObj.id === id) return { ...todoObj, done }
+      else return todoObj
+    })
+    this.setState({todos: newTodos})
+  }
+
   render() {
     const {todos} = this.state
     return (
       <div className = "todo-container">
           <div className="todo-wrap">
           <Header addTodo={this.addTodo}/>
-            <List todos={todos}/>
-            <Footer/>
+          <List todos={todos} updateTodo={this.updateTodo}/>
+          <Footer/>
           </div>
       </div>
     )
